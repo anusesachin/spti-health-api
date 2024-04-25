3b58e2ce764fe21461daefb16884b0a8fcca6273
Mohit Mahajan <mohit@vercity-india.com>
2024-04-25 16:42:38 +0530

    report backend

--- src/main/java/com/spti/dao/AdmitPatientRepository.java
+++ src/main/java/com/spti/dao/AdmitPatientRepository.java
@@ -1,27 +1,67 @@
 package com.spti.dao;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
-import com.spti.dto.patient.AdmitPatientResponseDto;
+import com.spti.dto.patient.PatientResponseDto;
 import com.spti.entity.AdmitPatient;
-import com.spti.entity.Patient;
+import com.spti.entity.PatientOPDHistory;
 
 @Repository
-public interface AdmitPatientRepository extends CrudRepository<AdmitPatient, Long>{
+public interface AdmitPatientRepository extends JpaRepository<AdmitPatient, Long> {
 
 	Optional<AdmitPatient> findByPatient_id(Long id);
 
-
-
 	Page<AdmitPatient> findByAdmitDischargeStatus(String string, Pageable pageable);
 
+	Optional<AdmitPatient> findByPatient_idAndAdmitDischargeStatus(Long id, String string);
 
+	@Query(value = "SELECT * FROM hospital.admit_patient WHERE DATE(admit_date) = :admissionDate", nativeQuery = true)
+	List<AdmitPatient> findByAdmissionDate(@Param("admissionDate") LocalDate admissionDate);
 
-    Optional<AdmitPatient> findByPatient_idAndAdmitDischargeStatus(Long id, String string);
+	@Query(value = "SELECT * FROM hospital.admit_patient WHERE YEAR(admit_date) = :year", nativeQuery = true)
+	List<AdmitPatient> findByYear(@Param("year") int year);
+
+	@Query(value = "SELECT * FROM hospital.admit_patient WHERE admit_date >= :startDate AND admit_date < :endDate", nativeQuery = true)
+	List<AdmitPatient> findByAdmitDateBetween(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+	
+	@Query(value = "SELECT * FROM hospital.admit_patient WHERE DATE(admit_date) = :date", nativeQuery = true)
+	List<AdmitPatient> findByPatientTodays(@Param("date") LocalDate date);
+
 
 }
+	
+	
+	
+	
+	
+	
+	
+	
+//@Query(value = "SELECT * FROM hospital.admit_patient WHERE admit_date >= :startDate AND admit_date < :endDatePlusOneDay", nativeQuery = true)
+//List<AdmitPatient> findByAdmitDateBetween(@Param("startDate") LocalDate startDate, @Param("endDatePlusOneDay") LocalDate endDatePlusOneDay);	
+
+//@Query( value = "SELECT * FROM hospital.admit_patient WHERE admit_date =:date", nativeQuery = true)
+//List<AdmitPatient> findByPatientTodays(@Param("date") LocalDate date);
+	
+//	@Query(value = "SELECT * FROM hospital.admit_patient WHERE admit_date BETWEEN :startDate AND :endDate", nativeQuery = true)
+//	List<AdmitPatient> findByDateRange(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+//   
+//	@Query(value = "SELECT * FROM hospital.admit_patient WHERE WEEK(admit_date, 1) = :week", nativeQuery = true)
+//    List<AdmitPatient> findByAdmitDateYearAndAdmitDateWeek( @Param("week") int week);
+
+//    SELECT * FROM hospital.admit_patient;
+//
+//    SELECT * FROM hospital.admit_patient WHERE DATE(admit_date) = '2024-04-15';
+//
+//    SELECT * FROM hospital.admit_patient where year(admit_date) = '2023';
+
--- src/main/java/com/spti/service/impl/AdmitPatientServiceImpl.java
+++ src/main/java/com/spti/service/impl/AdmitPatientServiceImpl.java
@@ -1,6 +1,10 @@
 package com.spti.service.impl;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.spti.dao.AdmitPatientRepository;
@@ -17,11 +22,14 @@ import com.spti.dao.PatientRepository;
 import com.spti.dao.TreatmentRepository;
 import com.spti.dto.patient.AdmitPatientRequestDto;
 import com.spti.dto.patient.AdmitPatientResponseDto;
+import com.spti.dto.patient.PatientOPDHistoryResponseDto;
+import com.spti.dto.patient.PatientResponseDto;
 import com.spti.dto.treatment.TreatmentRequest;
 import com.spti.dto.treatment.TreatmentResponse;
 import com.spti.entity.AdmitPatient;
 import com.spti.entity.Branch;
 import com.spti.entity.Patient;
+import com.spti.entity.PatientOPDHistory;
 import com.spti.entity.Treatment;
 import com.spti.mapper.patient.AdmitPatientMapper;
 import com.spti.mapper.patient.TreatmentMapper;
@@ -29,34 +37,34 @@ import com.spti.service.AdmitPatientService;
 
 @Service
 public class AdmitPatientServiceImpl implements AdmitPatientService {
-	
+
 	@Autowired
 	AdmitPatientMapper admitPatientMapper;
-	
+
 	@Autowired
 	AdmitPatientRepository admitPatientRepository;
-	
+
 	@Autowired
 	private PatientRepository patientRepository;
-	
+
 	@Autowired
 	private TreatmentMapper treatmentMapper;
-	
+
 	@Autowired
 	private TreatmentRepository treatmentRepository;
 
 	@Override
-	public boolean AdmitPatientAdd( AdmitPatientRequestDto dto) {
+	public boolean AdmitPatientAdd(AdmitPatientRequestDto dto) {
 		try {
-			AdmitPatient entity = admitPatientMapper.toEntity( dto );
-			
+			AdmitPatient entity = admitPatientMapper.toEntity(dto);
+
 			Optional<Patient> opt = patientRepository.findById(dto.getPatientId());
 			if (opt.isPresent()) {
 				entity.setPatient(opt.get());
-				admitPatientRepository.save( entity );
+				admitPatientRepository.save(entity);
 				return true;
 			}
-		} catch ( Exception e ) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
@@ -65,29 +73,31 @@ public class AdmitPatientServiceImpl implements AdmitPatientService {
 
 	@Override
 	public AdmitPatientResponseDto getAdmitPatientBypatienId(Long id) {
-		Optional<AdmitPatient> admitPatient = admitPatientRepository.findByPatient_idAndAdmitDischargeStatus( id ,"Admit" );
-		
-				if ( admitPatient.isPresent() )
-					return admitPatientMapper.toDto( admitPatient.get() );
-				else
-					return null;
-		
+		Optional<AdmitPatient> admitPatient = admitPatientRepository.findByPatient_idAndAdmitDischargeStatus(id,
+				"Admit");
+
+		if (admitPatient.isPresent())
+			return admitPatientMapper.toDto(admitPatient.get());
+		else
+			return null;
+
 	}
 
 	@Override
 	public Page<AdmitPatientResponseDto> allAdmitPatients(Pageable pageable) {
-		
-		Page<AdmitPatient> entityPage = admitPatientRepository.findByAdmitDischargeStatus("Admit"  ,pageable );
-		return new PageImpl<>( admitPatientMapper.toList( entityPage.getContent() ), pageable, entityPage.getTotalElements() );
+
+		Page<AdmitPatient> entityPage = admitPatientRepository.findByAdmitDischargeStatus("Admit", pageable);
+		return new PageImpl<>(admitPatientMapper.toList(entityPage.getContent()), pageable,
+				entityPage.getTotalElements());
 	}
 
 	@Override
 	public boolean addAdmittedPatientTreatmentDetails(List<TreatmentRequest> treatmentRequestdto) {
-		List<Treatment> treatmentList=new ArrayList<Treatment>();
-				treatmentRequestdto.forEach(treatment->{
-					treatmentList.add(treatmentMapper.toEntity(treatment));
-				});
-				
+		List<Treatment> treatmentList = new ArrayList<Treatment>();
+		treatmentRequestdto.forEach(treatment -> {
+			treatmentList.add(treatmentMapper.toEntity(treatment));
+		});
+
 		return treatmentRepository.saveAll(treatmentList) != null;
 	}
 
@@ -95,24 +105,71 @@ public class AdmitPatientServiceImpl implements AdmitPatientService {
 	public List<TreatmentResponse> getTreatmentDetailsByAdmittanceId(Long id) {
 		// TODO Auto-generated method stub
 		List<Treatment> treatmentList = treatmentRepository.findAllByAdmittanceId(id);
-		List<TreatmentResponse> treatmentResponseList=new ArrayList<TreatmentResponse>();
-		treatmentList.forEach(treatment->{
+		List<TreatmentResponse> treatmentResponseList = new ArrayList<TreatmentResponse>();
+		treatmentList.forEach(treatment -> {
 			treatmentResponseList.add(treatmentMapper.toDto(treatment));
 		});
-		
+
 		return treatmentResponseList;
 	}
 
-	
+	@Override
+	public List<PatientResponseDto> findByAdmissionDate(LocalDate admissionDate) {
+		List<AdmitPatient> admitPatientsDates = admitPatientRepository.findByAdmissionDate(admissionDate);
+		if (admitPatientsDates != null)
+			return admitPatientMapper.toPatientResponseDtoList(admitPatientsDates);
+		else
+			return null;
+	}
 
-	
+	@Override
+	public List<PatientResponseDto> findByYear(int year) {
+		List<AdmitPatient> admitPatientsByYear = admitPatientRepository.findByYear(year);
+		if (admitPatientsByYear != null)
+			return admitPatientMapper.toPatientResponseDtoList(admitPatientsByYear);
+		else
+			return null;
+	}
 
+	public List<PatientResponseDto> getPatientsBetweenDates(LocalDate startDate, LocalDate endDate) {
+		List<AdmitPatient> admitPatientsRandom = admitPatientRepository.findByAdmitDateBetween(startDate, endDate);
+		if (admitPatientsRandom != null)
+			return admitPatientMapper.toPatientResponseDtoList(admitPatientsRandom);
+		else
+			return null;
+	}
 
-
-	
-	
+	@Override
+	public List<PatientResponseDto> findAllPatient() {
+		List<AdmitPatient> patientResponseAllPateint= admitPatientRepository.findAll();
+		if (patientResponseAllPateint != null)
+			return admitPatientMapper.toPatientResponseDtoList(patientResponseAllPateint);
+		else
+			return null;
 		
+	}
 	
-
+	@Override
+	public List<PatientResponseDto> getListOfPatient(String todayrecord) {
+		if (todayrecord.equalsIgnoreCase("Today Patient")) {
+			LocalDate date = LocalDate.now();
+			List<AdmitPatient> entity = admitPatientRepository.findByPatientTodays(date);
+			return admitPatientMapper.toPatientResponseDtoList(entity);
+		} else if (todayrecord.equalsIgnoreCase("Weekly Patient")) {
+			LocalDate enddate = LocalDate.now();
+			LocalDate startDate = enddate.minusDays(7);
+			List<AdmitPatient> entityPage = admitPatientRepository.findByAdmitDateBetween(startDate, enddate);
+			return admitPatientMapper.toPatientResponseDtoList(entityPage);
+		} else if (todayrecord.equalsIgnoreCase("Monthly Patient")) {
+			LocalDate enddate = LocalDate.now();
+			LocalDate startDate = enddate.minusDays(31);
+			List<AdmitPatient> entityPage = admitPatientRepository.findByAdmitDateBetween(startDate, enddate);
+			return admitPatientMapper.toPatientResponseDtoList(entityPage);
+		} else {
+			
+			return Collections.emptyList();
+		}
+	}
+	
 
 }
